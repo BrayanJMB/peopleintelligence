c88bd1a1cb276481301153a5cc79a4e58fcf041e
OfficielSalah <officielsalah1970@gmail.com>
2022-10-09 05:08:33 +0000

    upload component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,12 @@ import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.bien}>
-      <h1>Bienvenue Dans Votre Compte Personnel</h1>
+    <div className={styles.screen}>
+      <div className={styles.box}>
+        <div className={styles.inner_box}>
+          <h1>Bienvenue Dans Votre Compte Personnel</h1>
+        </div>
+      </div>
     </div>
   );
 }
