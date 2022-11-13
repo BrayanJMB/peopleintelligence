216a38c165580e4d2421b730f156b916418cbeaa
OfficielSalah <officielsalah1970@gmail.com>
2022-11-13 21:31:51 +0000

    consume api token powerbi

--- src/pages/PowerBI/PowerBI.jsx
+++ src/pages/PowerBI/PowerBI.jsx
@@ -1,11 +1,56 @@
-import { PowerBIEmbed } from "powerbi-client-react";
-import { models } from "powerbi-client";
+import axios from "axios";
 import styles from "./PowerBI.module.css";
+import { PowerBIEmbed, report } from "powerbi-client-react";
+import { models } from "powerbi-client";
+import { useEffect, useState } from "react";
 import Navbar from "../../Layout/Navbar/Navbar";
 import Box from "@mui/material/Box";
 import Sidebar from "../../Layout/Sidebar/Sidebar";
 
-export default function PowerBI() {
+// Lifetime is 3600 sec/ 1 hour
+
+export default function PowerBi() {
+  const [response, setResponse] = useState("");
+  let token = response?.powerBiEmbedToken?.token;
+  let embedUrl = response?.powerBiReport?.embedUrl;
+  let id = response?.powerBiReport?.id;
+  let accessToken = async () => {
+    console.log("run function");
+    try {
+      const response = await axios
+        .create({
+          baseURL: `https://dynamicliveconversationapi.azurewebsites.net/api/PowerBy`,
+        })
+        .get("/1");
+      setResponse(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  /*let powerbiEmbededtoken = async () => {
+    try {
+      const response = await axios
+        .create({
+          baseURL: `https://${powerBI_API_URL}/v1.0/myorg/groups/${groupId}/reports/${reportId}/GenerateToken`,
+        })
+        .post("", data, config);
+      console.log(response.data.token);
+    } catch {}
+  };*/
+
+  useEffect(() => {
+    if (response) {
+      let timer1 = setInterval(() => accessToken(), 1000 * 60 * 60);
+      return () => {
+        clearInterval(timer1);
+      };
+    } else {
+      accessToken();
+    }
+
+    //powerbiEmbededtoken();
+  }, [response]);
+
   return (
     <Box sx={{ display: "flex" }}>
       <Navbar />
@@ -15,11 +60,10 @@ export default function PowerBI() {
           <PowerBIEmbed
             embedConfig={{
               type: "report", // Supported types: report, dashboard, tile, visual and qna
-              id: "fe404000-c09b-4576-9d98-37c5cbc3db49",
-              embedUrl:
-                "https://app.powerbi.com/reportEmbed?reportId=fe404000-c09b-4576-9d98-37c5cbc3db49&groupId=9e74cc56-feaf-4719-887b-40b66e4828fb&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVBBQVMtMS1TQ1VTLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWUsInNraXBRdWVyeURhdGFTYWFTRW1iZWQiOnRydWUsInNraXBRdWVyeURhdGFQYWFTRW1iZWQiOnRydWUsInNraXBRdWVyeURhdGFFeHBvcnRUbyI6dHJ1ZX19",
-              accessToken:"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvY2Y0NDc5MWItNTU0YS00MmI0LWFjOTMtNTdjZTQ1ZWFhZDAxLyIsImlhdCI6MTY2NjMwMjg1NSwibmJmIjoxNjY2MzAyODU1LCJleHAiOjE2NjYzMDgzMDIsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VEFBQUF3YzVDV21XeS8vMUVKL2crTm1UMzVsa3o0TXRGdEp3VWI5M1JmRWZsTTh0c1Evdy9xUkhtMWdjQjBNMXBUVEQ5SGZsdXBtRlNTYlk0d2dXV2lSTTU4SEs4bWIwQThTWUY4cUVlZlg5bCtGbz0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiODcxYzAxMGYtNWU2MS00ZmIxLTgzYWMtOTg2MTBhN2U5MTEwIiwiYXBwaWRhY3IiOiIyIiwiZmFtaWx5X25hbWUiOiJCYXJjbyBHb21leiIsImdpdmVuX25hbWUiOiJTYW50aWFnbyIsImlwYWRkciI6IjE4MS41OC41My4yNTMiLCJuYW1lIjoiU2FudGlhZ28gQmFyY28gR29tZXoiLCJvaWQiOiJkY2I4M2Y1Mi02YTJmLTRhYWMtYmRlZC1iODhkMDUwZTUzYjkiLCJwdWlkIjoiMTAwMzIwMDIxQ0I5ODI4OSIsInJoIjoiMC5BWDBBRzNsRXowcFZ0RUtzazFmT1JlcXRBUWtBQUFBQUFBQUF3QUFBQUFBQUFBQ2NBRGsuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoidzJBWUJRcW4xVDc3Wnp4Rmo0NUZLQkEzRWRIVmhLM09zYUFkTzVFSEctRSIsInRpZCI6ImNmNDQ3OTFiLTU1NGEtNDJiNC1hYzkzLTU3Y2U0NWVhYWQwMSIsInVuaXF1ZV9uYW1lIjoiU2FudGlhZ29CYXJjb0dvbWV6QERTNFQub25taWNyb3NvZnQuY29tIiwidXBuIjoiU2FudGlhZ29CYXJjb0dvbWV6QERTNFQub25taWNyb3NvZnQuY29tIiwidXRpIjoiX2Q4V0FZMUJEazI3dHpmLVVIVkNBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiNjJlOTAzOTQtNjlmNS00MjM3LTkxOTAtMDEyMTc3MTQ1ZTEwIiwiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.APc58FJRAUJZRInTm8tXCWmMSVweaRhHMNqm7Qq18tyD4uMNZqOwHUCImI-gyjURV10n87PZON8_iQdNTBwP57m-q7USRs9IBmruCmJqeWiOUE24FcK_KiBuGjk9s7uPSUI5yKD48NooJEsPyoMt7KIQBN6290y0sIwtqndkItBOdvt_uce0IqLRoxDQgUZwOc39Lu9Yl-Rm36T6VmkRM1Zb0BX-MWEVxfZt2xa2IdUx7Fat6HmTohHkYl9fZYXDOoxzL3gabhMheT7QXnaPn7dh0_4VE0kGKsWk2i3agYU_l_HU1MT1kaoOC_6iT_V3CZvZBiuNh3Sjlw83nNzAeQ",
-              tokenType: models.TokenType.Add,
+              id: id,
+              embedUrl: embedUrl,
+              accessToken: token,
+              tokenType: models.TokenType.Embed,
               settings: {
                 panes: {
                   filters: {
@@ -27,7 +71,6 @@ export default function PowerBI() {
                     visible: false,
                   },
                 },
-                background: models.BackgroundType.Transparent,
               },
             }}
             eventHandlers={
