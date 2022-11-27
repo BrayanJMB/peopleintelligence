567d4f47672d14b240b5d447241946f84a491d46
OfficielSalah <officielsalah1970@gmail.com>
2022-11-27 23:40:29 +0000

    fix multiple phone regex

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,7 +1,17 @@
 import { Outlet, Navigate } from "react-router-dom";
+import { getCompaniesAPI } from "../services/getCompanies.service";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function PrivateRoutes() {
+  const location = useLocation();
+  const navigate = useNavigate();
   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  getCompaniesAPI(userInfo?.user).then((res) => {
+    if (res.data.length === 0 && location.pathname !== "/infoadmin/Empresas") {
+      alert("No tienes una compañía registrada, por favor registra una.");
+      navigate("/infoadmin/Empresas");
+    }
+  });
 
   return userInfo && userInfo.role.findIndex((p) => p === "Registrado") < 0 ? (
     <Outlet />
