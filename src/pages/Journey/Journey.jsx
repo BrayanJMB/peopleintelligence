74959558d4506d8e8486e87f732a495a68cc6fc2
Fernando López Moreno <koficoud@gmail.com>
2023-02-19 17:55:25 -0600

    fix: :bug: update personal criteria

--- src/pages/Journey/Journey.jsx
+++ src/pages/Journey/Journey.jsx
@@ -182,7 +182,7 @@ export default function Journey() {
               </div>
 
               <div className={styles.templates}>
-                {journeys.filter((journey) => !journey.ispersonal).map((journey) => {
+                {journeys.filter((journey) => journey.ispersonal).map((journey) => {
                   return (
                     <div key={journey.id} className={styles.card}>
                       <img
@@ -225,7 +225,7 @@ export default function Journey() {
                     </div>
                   );
                 })}
-                {journeys.filter((journey) => journey.ispersonal).map((journey) => {
+                {journeys.filter((journey) => !journey.ispersonal).map((journey) => {
                   return (
                     <div key={journey.id} className={styles.encuestacard} onClick={() => navigate(`/journey/survey/${journey.id}`)}>
                       <div
