1b713ad9ecad975d344f5044cbbd1aa44647ff9d
Fernando López Moreno <koficoud@gmail.com>
2023-02-19 16:44:31 -0600

    feat: :sparkles: add new endpoint

--- src/services/getJourneyMap.service.js
+++ src/services/getJourneyMap.service.js
@@ -1,3 +1,12 @@
 import axios from "../utils/axiosInstance";
 
 export const getJourneyMapAPI = async () => axios.get("GetJorneyMap/");
+
+/**
+ * Get Journeys by company and map.
+ *
+ * @param companyId
+ * @param mapId
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const getJourneysCompanyAPI = async (companyId, mapId) => axios.get(`GetJorneysCompany/${companyId}/${mapId}`);
