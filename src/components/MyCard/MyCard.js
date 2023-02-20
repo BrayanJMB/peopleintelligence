e4644b3b54f9eaab2e41dd5d67d8381e36135151
Fernando López Moreno <koficoud@gmail.com>
2023-02-20 16:20:08 -0600

    feat: :sparkles: create my card component

---
+++ src/components/MyCard/MyCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './MyCard.module.css';
+
+/**
+ * My Card Component.
+ *
+ * @param children
+ * @param style
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const MyCard = ({ children }) => (
+  <div className={styles.MyCard}>
+    {children}
+  </div>
+);
+
+MyCard.propTypes = {
+  children: PropTypes.node,
+};
+
+MyCard.defaultProps = {
+  children: null,
+};
+
+export default MyCard;
