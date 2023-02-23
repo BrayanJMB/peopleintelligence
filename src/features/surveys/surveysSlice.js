e1b474de107909f28500250309be3b9b53903db2
Fernando López Moreno <koficoud@gmail.com>
2023-02-22 22:24:44 -0600

    feat: :sparkles: implement fetch survey for answer

--- src/features/surveys/surveysSlice.js
+++ src/features/surveys/surveysSlice.js
@@ -3,6 +3,7 @@ import client from '../../utils/axiosInstance';
 
 const initialState = {
   currentSurvey: null,
+  currentSurveyForAnswer: null,
   status: 'idle',
   error: null,
 };
@@ -14,28 +15,54 @@ export const fetchSurveyByIdAndCompanyId = createAsyncThunk(
 
     return data;
   }
-)
+);
+
+export const fetchSurveyForAnswer = createAsyncThunk(
+  'surveys/fetchSurveyForAnswer',
+  async ({ surveyId, companyId }) => {
+    const { data } = await client.get(`ShowQuestionAnswer/${surveyId}/${companyId}`)
+
+    return data;
+  }
+);
 
 const surveysSlice = createSlice({
   name: 'surveys',
   initialState,
   reducers: {},
-  extraReducers: {
-    [fetchSurveyByIdAndCompanyId.pending]: (state, action) => {
-      state.status = 'loading';
-    },
-    [fetchSurveyByIdAndCompanyId.fulfilled]: (state, action) => {
-      state.status = 'succeeded';
-      state.currentSurvey = action.payload;
-    },
-    [fetchSurveyByIdAndCompanyId.rejected]: (state, action) => {
-      state.status = 'failed';
-      state.error = action.error.message;
-    },
-  }
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSurveyByIdAndCompanyId.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchSurveyByIdAndCompanyId.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.currentSurvey = action.payload;
+      } )
+      .addCase(fetchSurveyByIdAndCompanyId.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      }
+    );
+
+    builder
+      .addCase(fetchSurveyForAnswer.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchSurveyForAnswer.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.currentSurveyForAnswer = action.payload;
+      })
+      .addCase(fetchSurveyForAnswer.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
+
 export const selectCurrentSurvey = (state) => state.surveys.currentSurvey;
+export const selectCurrentSurveyForAnswer = (state) => state.surveys.currentSurveyForAnswer;
 export const selectSurveysStatus = (state) => state.surveys.status;
 
 export default surveysSlice.reducer;
