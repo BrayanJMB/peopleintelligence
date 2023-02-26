f00093223c825c5b50b4420bffd768103ea46a39
Fernando López Moreno <koficoud@gmail.com>
2023-02-25 19:51:07 -0600

    feat: :sparkles: create send invitation dialog component

---
+++ src/pages/SurveyDetailPage/components/SendInvitationDialog/SendInvitationDialog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './SendInvitationDialog.module.css';
+
+const SendInvitationDialog = () => (
+  <div className={styles.SendInvitationDialog}>
+    SendInvitationDialog Component
+  </div>
+);
+
+SendInvitationDialog.propTypes = {};
+
+SendInvitationDialog.defaultProps = {};
+
+export default SendInvitationDialog;
