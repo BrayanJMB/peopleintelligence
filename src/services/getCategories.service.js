5a3071cdf94b3d02c183fb47c03628b5fa99c960
Fernando López Moreno <koficoud@gmail.com>
2023-03-09 21:00:42 -0600

    feat: :sparkles: add CRUD endpoints

--- src/services/getCategories.service.js
+++ src/services/getCategories.service.js
@@ -1,3 +1,33 @@
 import client from "../utils/axiosInstance";
 
+/**
+ * Fetch categories from API.
+ *
+ * @returns {Promise<AxiosResponse<any>>}
+ */
 export const fetchCategoriesAPI = async () => client.get('Categories/');
+
+/**
+ * Update category from API.
+ *
+ * @param id
+ * @param data
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const updateCategoryAPI = async (id, data) => client.put(`Categories/${id}`, data);
+
+/**
+ * Delete category from API.
+ *
+ * @param id
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const deleteCategoryAPI = async (id) => client.delete(`Categories/${id}`);
+
+/**
+ * Store category from API.
+ *
+ * @param data
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const storeCategoryAPI = async (data) => client.post('Categories/', data);
