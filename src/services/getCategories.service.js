76d4687d2ea9ded0e30b11074aa5ba0496de37f1
Fernando López Moreno <koficoud@gmail.com>
2023-03-10 14:25:59 -0600

    feat: :sparkles: update params

--- src/services/getCategories.service.js
+++ src/services/getCategories.service.js
@@ -3,7 +3,7 @@ import client from "../utils/axiosInstance";
 /**
  * Fetch categories from API.
  *
- * @returns {Promise<AxiosResponse<any>>}
+ * @returns {Promise<any>}
  */
 export const fetchCategoriesAPI = async () => client.get('Categories/');
 
@@ -12,15 +12,15 @@ export const fetchCategoriesAPI = async () => client.get('Categories/');
  *
  * @param id
  * @param data
- * @returns {Promise<AxiosResponse<any>>}
+ * @returns {Promise<any>}
  */
-export const updateCategoryAPI = async (id, data) => client.put(`Categories/${id}`, data);
+export const updateCategoryAPI = async ({id, ...data}) => client.put(`Categories/${id}`, data);
 
 /**
  * Delete category from API.
  *
  * @param id
- * @returns {Promise<AxiosResponse<any>>}
+ * @returns {Promise<any>}
  */
 export const deleteCategoryAPI = async (id) => client.delete(`Categories/${id}`);
 
@@ -28,6 +28,6 @@ export const deleteCategoryAPI = async (id) => client.delete(`Categories/${id}`)
  * Store category from API.
  *
  * @param data
- * @returns {Promise<AxiosResponse<any>>}
+ * @returns {Promise<any>}
  */
 export const storeCategoryAPI = async (data) => client.post('Categories/', data);
