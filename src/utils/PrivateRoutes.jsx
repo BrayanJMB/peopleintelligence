ff25edc49787bd3fedb4d21acc0748b94825c6a9
Brayan Milian <milian.brayanc@gmail.com>
2023-08-21 11:47:36 -0500

    ' was added for interpoaltion string

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -14,7 +14,7 @@ export default function PrivateRoutes() {
   const userInfo = JSON.parse(localStorage.getItem('userInfo'));
   if (!userInfo) {
     window.location.replace(
-      'https://peopleintelligenceb2c.b2clogin.com/peopleintelligenceb2c.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_sisu&client_id=a6ae19dc-57c8-44ce-b8b9-c096366ba4a2&nonce=defaultNonce&redirect_uri=https%3A%2F%2F${process.env.DOMAINB2C}.peopleintelligence.app&scope=https%3A%2F%2Fpeopleintelligenceb2c.onmicrosoft.com%2Fa6ae19dc-57c8-44ce-b8b9-c096366ba4a2%2FFiles.Read&response_type=token&prompt=login'
+      `https://peopleintelligenceb2c.b2clogin.com/peopleintelligenceb2c.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_sisu&client_id=a6ae19dc-57c8-44ce-b8b9-c096366ba4a2&nonce=defaultNonce&redirect_uri=https%3A%2F%2F${process.env.DOMAINB2C}.peopleintelligence.app&scope=https%3A%2F%2Fpeopleintelligenceb2c.onmicrosoft.com%2Fa6ae19dc-57c8-44ce-b8b9-c096366ba4a2%2FFiles.Read&response_type=token&prompt=login`
       );
 
   }
