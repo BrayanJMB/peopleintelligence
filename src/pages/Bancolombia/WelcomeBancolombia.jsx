d1ebb291a95a8224786ce1c6c4fb5c725f531e3b
Brayan Milian <milian.brayanc@gmail.com>
2024-02-05 12:18:19 -0500

    negrillas was implemented

--- src/pages/Bancolombia/WelcomeBancolombia.jsx
+++ src/pages/Bancolombia/WelcomeBancolombia.jsx
@@ -20,6 +20,24 @@ export const WelcomeBancolombia = ({
   const mobileStyle = {
     backgroundImage: 'none',
   };
+  const convertirLineas = (texto) => {
+    // Dividir el texto en líneas y luego procesar cada línea
+    return texto.split('\n').map((linea, index) => {
+      // Reemplazar las etiquetas <br> por elementos JSX en negrita
+      const partes = linea.split(/<br>|<\/br>/).map((parte, idx) => {
+        if (idx % 2 === 1) {
+          // Las partes que estaban entre las etiquetas <br>
+          return <strong key={idx}>{parte}</strong>;
+        } else {
+          // Las demás partes
+          return parte;
+        }
+      });
+
+      // Devolver la línea como un párrafo con las partes procesadas
+      return <p key={index} className={styles.mb}>{partes}</p>;
+    });
+  };
   return (
     <Layout>
       <div
@@ -37,9 +55,7 @@ export const WelcomeBancolombia = ({
           {' '}
           {/* Ajusta el valor como sea necesario */}
           <h3 className={styles.mb}>{titulo}</h3>
-          {descripcion.split('\n').map((linea, index) => (
-            <p className={styles.mb}>{linea}</p>
-          ))}
+            <p className={styles.mb}>{convertirLineas(descripcion)}</p>
         </div>
         <NextQuestion
           handleNext={handleNext}
