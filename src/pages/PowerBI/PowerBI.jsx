126223dd4b2ff7f8f8fdba3b0a76285a67fa325a
Brayan Milian <milian.brayanc@gmail.com>
2024-04-10 12:21:16 -0500

    fix: reuse token for powerBI Dashboard

--- src/pages/PowerBI/PowerBI.jsx
+++ src/pages/PowerBI/PowerBI.jsx
@@ -1,90 +1,102 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import { models } from 'powerbi-client';
-import { PowerBIEmbed } from 'powerbi-client-react';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import { models } from "powerbi-client";
+import { PowerBIEmbed } from "powerbi-client-react";
 
-import IconSidebar from '../../Layout/IconSidebar/IconSidebar';
-import Navbar from '../../Layout/Navbar/Navbar';
-import axios from '../../utils/axiosInstance';
+import IconSidebar from "../../Layout/IconSidebar/IconSidebar";
+import Navbar from "../../Layout/Navbar/Navbar";
+import axios from "../../utils/axiosInstance";
 
-import styles from './PowerBI.module.css';
+import styles from "./PowerBI.module.css";
 // Lifetime is 3600 sec/ 1 hour
 export default function PowerBi() {
-  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-  const [response, setResponse] = useState('');
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const [response, setResponse] = useState("");
   const navigate = useNavigate();
   const { idDashboard } = useParams();
-  const [userEmail, setUserEmail] = useState('');
+  const [userEmail, setUserEmail] = useState("");
   const decodeToken = (token) => {
-    var base64Url = token.split('.')[1];
+    var base64Url = token.split(".")[1];
     var base64 = decodeURIComponent(
       atob(base64Url)
-        .split('')
+        .split("")
         .map((c) => {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
         })
-        .join('')
+        .join("")
     );
     return JSON.parse(base64);
   };
   // Creamos una tabla
   const accessToken = async () => {
-    console.log('entro aca');
+    const storedResponses =
+      JSON.parse(localStorage.getItem("powerBiResponses")) || {};
+    const currentTime = new Date().getTime();
+
+    if (storedResponses[idDashboard]) {
+      const { expiry, ...rest } = storedResponses[idDashboard];
+      if (currentTime < expiry) {
+        setResponse(rest);
+        return; // Token válido para el dashboard actual, no necesitamos hacer una nueva solicitud
+      } else {
+        // Opcional: Proceder a eliminar el token expirado si se desea
+        delete storedResponses[idDashboard];
+        localStorage.setItem("powerBiResponses", JSON.stringify(storedResponses));
+      }
+    }
+
     try {
-      await axios.get('PowerBy/' + idDashboard).then((res) => {
-        if (
-          res.data.powerBiEmbedToken === null ||
-          res.data.powerBiReport === null
-        ) {
-          alert('El reporte no existe o esta desahabilitado');
-          navigate('/powerbi');
-        }
-        setResponse({
+      const res = await axios.get("PowerBy/" + idDashboard);
+      if (
+        res.data.powerBiEmbedToken === null ||
+        res.data.powerBiReport === null
+      ) {
+        alert("El reporte no existe o está deshabilitado");
+        navigate("/powerbi");
+      } else {
+        const responseToStore = {
           token: res.data.powerBiEmbedToken?.token,
           id: res.data.powerBiReport?.id,
           embedUrl: res.data.powerBiReport?.embedUrl,
-        });
-      });
+          expiry: new Date().getTime() + 1000 * 60 * 60, // Asume duración de token de 1 hora
+        };
+        storedResponses[idDashboard] = responseToStore;
+        localStorage.setItem(
+          "powerBiResponses",
+          JSON.stringify(storedResponses)
+        );
+        setResponse(responseToStore);
+      }
     } catch (e) {
-      if (e.response.status === 400) {
-        alert('Este dashborad no esta habilitado');
-        navigate('/powerbi');
+      if (e.response && e.response.status === 400) {
+        alert("Este dashboard no está habilitado");
+        navigate("/powerbi");
       }
     }
   };
 
-
   useEffect(() => {
-    console.log('entro aca2');
-    if (userInfo.role.findIndex((p) => p === 'PowerBiDashboard') > -1) {
-      if (response) {
-        let timer1 = setInterval(() => accessToken(), 1000 * 60 * 60);
-        return () => {
-          clearInterval(timer1);
-        };
-      } else {
-        console.log('entro a consumir de neuvo');
-        accessToken();
-      }
+    if (userInfo.role.findIndex((p) => p === "PowerBiDashboard") > -1) {
+      accessToken();
     } else {
-      alert('No tiene permiso para acceder a esta funcionalidad');
-      navigate('/dashboard');
+      alert("No tiene permiso para acceder a esta funcionalidad");
+      navigate("/dashboard");
     }
+    // Esto asume que decodeToken es una función sincrónica que decodifica el accessToken del usuario.
     setUserEmail(decodeToken(userInfo.accessToken));
-    console.log(response);
-  }, [response]);
-   
+  }, []); // Dependencias vacías para ejecutar solo en montaje
+
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <Navbar />
       <IconSidebar />
-      <div style={{ backgroundColor: 'white' }}>
+      <div style={{ backgroundColor: "white" }}>
         <div className={styles.content}>
           <PowerBIEmbed
             embedConfig={{
-              type: 'report', // Supported types: report, dashboard, tile, visual and qna
+              type: "report", // Supported types: report, dashboard, tile, visual and qna
               id: response?.id,
               embedUrl: response?.embedUrl,
               accessToken: response?.token,
@@ -99,12 +111,12 @@ export default function PowerBi() {
               },
               filters: [
                 {
-                  $schema: 'http://powerbi.com/product/schema#basic',
+                  $schema: "http://powerbi.com/product/schema#basic",
                   target: {
-                    table: 'z_RLS', // Nombre de la tabla
-                    column: 'user_name', // Nombre de la columna a buscar
+                    table: "z_RLS", // Nombre de la tabla
+                    column: "user_name", // Nombre de la columna a buscar
                   },
-                  operator: 'In', //Forma de busqueda
+                  operator: "In", //Forma de busqueda
                   values: [userEmail.email], //Este es el valor a buscar
                 },
               ],
@@ -112,19 +124,19 @@ export default function PowerBi() {
             eventHandlers={
               new Map([
                 [
-                  'loaded',
+                  "loaded",
                   function () {
-                    console.log('Report loaded');
+                    console.log("Report loaded");
                   },
                 ],
                 [
-                  'rendered',
+                  "rendered",
                   function () {
-                    console.log('Report rendered');
+                    console.log("Report rendered");
                   },
                 ],
                 [
-                  'error',
+                  "error",
                   function (event) {
                     console.log(event.detail);
                   },
