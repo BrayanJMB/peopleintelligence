00d3f05085bb4539d353ecf9d5aa71ca4da2d0c0
Brayan Milian <milian.brayanc@gmail.com>
2025-06-06 02:20:03 -0500

    styles / nps for js convention

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -7,7 +7,7 @@ import companiesSlice from '../features/companies/companiesSlice';
 import activeCompaniesSlice from '../features/employe/employe';
 import powerBiReducer from '../features/powerBiSlice';
 import surveysSlice from '../features/surveys/surveysSlice';
-import token360Slice from '../features/token360Slice'
+import token360Slice from '../features/token360Slice';
 export const store = configureStore({
   reducer: {
     admin: adminReducer,
@@ -17,7 +17,7 @@ export const store = configureStore({
     surveys : surveysSlice,
     carousel  : CarouselSlice,
     activeCompanies : activeCompaniesSlice,
-    token360: token360Slice
+    token360: token360Slice,
   },
   devTools: true,
 });
